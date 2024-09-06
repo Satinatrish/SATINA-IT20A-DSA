dc5e8f050872e634065aa4e8a49a0c62c6be4d0f
Satinatrish <trishaannsatina@gmail.com>
2024-09-06 11:04:17 +0800

    added activity

---
+++ script.js
@@ -0,0 +1,9 @@
+// activity 
+
+let firstname = "Trisha";
+let lastname = "Satina";
+
+console.log("Hello " + firstname + " " + lastname + " Welcome to IT21a ");
+alert("Hello " + firstname + " " + lastname + " Welcome to IT21a ");
+
+document.getElementById("text").innerHTML =  "Hello " + firstname + " " + lastname + " Welcome to IT21a " ;
