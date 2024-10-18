972ade163d9c9644f9ceec8a387aa8a25d127ca2
TRISHA ANN SATINA <trishaannsatina@gmail.com>
2024-10-18 10:38:00 +0800

    added linked list

---
+++ Linkedlist.java
@@ -0,0 +1,32 @@
+
+package linkedlist;
+import java.util.LinkedList;
+
+public class Linkedlist {
+
+  
+    public static void main(String[] args) {
+         LinkedList<String> animals = new LinkedList<>();
+
+
+        animals.add("Dog");
+        animals.addFirst("Cat");
+        animals.addLast("Elephant");
+        animals.add(1, "Lion");
+
+        System.out.println("Linked List Original: " + animals);
+        System.out.println(animals.size());
+
+
+        if (animals.contains("Lion")) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+        boolean containslion = animals.contains("Lion");
+        System.out.println(containslion);
+
+    }
+    
+}
